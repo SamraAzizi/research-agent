255cfb29cf86247a251a7006f6ce89553059b60b
SamraAzizi <samarraazizi@gmail.com>
2025-06-18 22:33:52 +0500

    advnaced research src

---
+++ src/firecrawl.py
@@ -0,0 +1,13 @@
+import os
+from firecrawl import FirecrawlApp, ScrapeOptions
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class FirecrawlService:
+    def __init__(self):
+        api_key = os.getenv("FIRECRAWL_API_KEY")
+        if not api_key:
+            raise ValueError("Missing FIRECRAWL_API_KEY environment variable")
+        self.app = FirecrawlApp(api_key=api_key)
