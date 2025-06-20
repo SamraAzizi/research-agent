8c0ddf92505214d31d54619f5e43d768ef7c2c5d
SamraAzizi <samarraazizi@gmail.com>
2025-06-20 14:56:47 +0500

    advnaced research src

--- src/workflow.py
+++ src/workflow.py
@@ -5,3 +5,21 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from .models import ResearchState, CompanyInfo, CompanyAnalysis
 from .firecrawl import FirecrawlService
 from .prompts import DeveloperToolsPrompts
+
+class Workflow:
+    def __init__(self):
+        self.firecrawl = FirecrawlService()
+        self.llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.1)
+        self.prompts = DeveloperToolsPrompts()
+        self.workflow = self._build_workflow()
+
+    def _build_workflow(self):
+        graph = StateGraph(ResearchState)
+        graph.add_node("extract_tools", self._extract_tools_step)
+        graph.add_node("research", self._research_step)
+        graph.add_node("analyze", self._analyze_step)
+        graph.set_entry_point("extract_tools")
+        graph.add_edge("extract_tools", "research")
+        graph.add_edge("research", "analyze")
+        graph.add_edge("analyze", END)
+        return graph.compile()
