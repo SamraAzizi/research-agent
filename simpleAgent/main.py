9bd87a57a1e377d47f87b0d4af433928994ba03a
SamraAzizi <samarraazizi@gmail.com>
2025-06-22 11:22:29 +0500

    simple agent

--- simpleAgent/main.py
+++ simpleAgent/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from simpleagent!")
-
-
-if __name__ == "__main__":
-    main()
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import asyncio
+import os
